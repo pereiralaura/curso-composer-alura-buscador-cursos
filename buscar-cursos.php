9ebe4cfe3197f899ba19ee1b56b7b9842821101b
Laura Pereira <lagpereira28@gmail.com>
2025-11-14 13:10:50 -0300

    Commit: Buscador de cursos

---
+++ buscar-cursos.php
@@ -0,0 +1,19 @@
+<?php
+
+require 'vendor/autoload.php';
+
+
+use Alura\BuscadorDeCursos\Buscador;
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+//Cria um 'client' e faz uma requisição de uma url base
+$client = new Client(['base_uri' => 'https://alura.com.br/']);
+$crawler = new Crawler();
+
+$buscador = new Buscador($client, $crawler);
+$cursos = $buscador->buscar('/cursos-online-programacao/php');
+
+foreach ($cursos as $curso) {
+    echo $curso . PHP_EOL;
+}
